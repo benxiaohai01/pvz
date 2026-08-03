83c3ec26fd1f2ed2ee3640899f86a6bbaeeea559
benxiaohai <zhouga@asiainfo.com>
2026-08-03 22:14:35 +0800

    重构

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.bxh.pvz {
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.base;
-    requires java.desktop;
-    requires java.logging;
-
-    exports org.bxh.pvz.bootstrap;
-}
--- src/main/java/org/bxh/pvz/config/WaveData.java
+++
@@ -1,5 +0,0 @@
-package org.bxh.pvz.config;
-
-import java.util.List;
-
-public record WaveData(int zombieCount, double spawnInterval, double delayBeforeWave) {}
